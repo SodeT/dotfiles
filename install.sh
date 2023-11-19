b208b65be7a4479361dc182191f158939bc23cd1
SodeT <sode.timmeras@gmail.com>
2023-11-19 14:06:32 +0100

    fix?

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
+
 pacman -Suy alacritty btop doas vim neovim nodejs-emojione ttf-jetbrains-mono ttf-font-awesome zsh sway
 
+# oh my zsh install
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# nvim packer install
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
